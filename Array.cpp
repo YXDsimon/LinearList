880a1f037234e435dbaa47a46f1ab51751c3556e
Simon <1071939594@qq.com>
2020-10-20 23:08:29 +0800

    finished part2

--- Array.cpp
+++ Array.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class ArrayList
 {
 private:
-    int List[100];
+    string List;
     int len;
 
 public:
@@ -13,67 +13,17 @@ public:
     {
         for (int i = 0; i < LenOfInput; i++)
         {
-            List[i] = input[i];
+            List += input[i];
         }
         len = LenOfInput;
     }
-    bool insert_list(int index, string val)
-    {
-        if (index < 0 || index > len)
-        {
-            cout << "illegal index!" << endl;
-            return 0;
-        }
-        for (int i = len + 1; i > index; i--)
-        {
-            List[i] = List[i - 1];
-            List[i - 1] = val;
-        }
-        len++;
-        return 1;
-    }
-    int delete_list(int index)
-    {
-        if (index < 0 || index > len)
-        {
-            cout << "illegal index!" << endl;
-            return 0;
-        }
-        for (int i = index; i < len - 1; i++)
-        {
-            List[i] = List[i + 1];
-        }
-        len--;
-    }
     void print()
     {
-        cout << "The List is:" << endl;
-        for (int i = 0; i < len; i++)
-        {
-            cout << List[i];
-            if (i == len - 1)
-            {
-                cout << endl;
-            }
-            else
-            {
-                cout << ' ';
-            }
-        }
+        cout << "The List is:" << List << endl;
     }
-    ArrayList operator+(const ArrayList &b)
+    string operator+(const ArrayList &b)
     {
-        string list;
-        for (int i = 0; i < this->len; i++)
-        {
-            list[i] = this->List[i];
-        }
-        for (int i = 0; i < b.len; i++)
-        {
-            list[i + this->len] = b.List[i];
-        }
-        ArrayList c(list, this->len + b.len);
-        return c;
+        return this->List +' '+ b.List;
     }
 };
 
@@ -81,6 +31,7 @@ int main()
 {
     string a = "I love";
     string b = "Data Structure";
-    cout<<sizeof(a)/sizeof(a[0]);
-    ArrayList array1(a,sizeof(a)/sizeof(a[0])) ;
+    ArrayList array1(a, a.length());
+    ArrayList array2(b, b.length());
+    cout << array1 + array2 << endl;
 }
